a088cfa59875c55730f31bb6eda667cace12d342
HaiCauTapCode <parkourdqk@gmail.com>
2024-04-24 20:12:02 +0700

    fix jdbcDTO and update file pom.xml

--- Airplane_Ticket/src/main/java/DAO/BaseDAO.java
+++ Airplane_Ticket/src/main/java/DAO/BaseDAO.java
@@ -1,10 +1,8 @@
 package DAO;
 
-import DTO.MayBayDTO;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BaseDAO {
     private static String jdbcURL = "jdbc:mysql://localhost:3307/ticketmanager";   // tên database
